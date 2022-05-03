968a990d1849ba502f0e54fcce9c99aafc98cfb8
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-05-03 09:55:19 -0500

    create event functionality

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { getEvents } from "./EventManager.js"
+import { useHistory } from "react-router-dom"
 
 export const EventList = (props) => {
   const [events, setEvents] = useState([])
@@ -9,17 +10,26 @@ export const EventList = (props) => {
   }, [])
 
   return (
-    <article className="events">
-      {
-        events.map(event => {
-          return <section key={`event--${event.id}`} className="event">
-            <div className="event__game">{event.game.title} Skill Level: {event.game.skill_level}</div>
-            <div className="event__description">{event.description}</div>
-            <div className="date">{event.date}</div>
-            <div className="event__time">{event.time}</div>
-          </section>
-        })
-      }
-    </article>
+    <>
+      <article className="events">
+        {
+          events.map(event => {
+            return <section key={`event--${event.id}`} className="event">
+              <div className="event__game">{event.game.title} Skill Level: {event.game.skill_level}</div>
+              <div className="event__description">{event.description}</div>
+              <div className="date">{event.date}</div>
+              <div className="event__time">{event.time}</div>
+            </section>
+          })
+        }
+      </article>
+      <button className="btn btn-2 btn-sep icon-create"
+        onClick={() => {
+          history.push("/events/new")
+        }}
+      >
+        Register New Event
+      </button>
+    </>
   )
 }
